c5fa087dabbcc9bfffc5893b4a52b6bf7626f57e
Mike Champion <championmike42@gmail.com>
2017-10-12 20:07:13 -0500

    final working commit

--- main.py
+++ main.py
@@ -8,43 +8,58 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
-
+class Bloggz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
+    title = db.Column(db.String(75))
+    content = db.Column(db.String(500))
 
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
 
+
+
+@app.route('/blog_submit', methods=['POST', 'GET'])
+def blog_submit():
+    title = ''
+    content = ''
+    valid=True
+    title_error=''
+    content_error=''
+    
+    if request.method == 'POST':
+        title = request.form['blog-title']
+        content = request.form['blog-content']
+        
+        if title=='':
+            title_error="""Don't you want to title your Blog post?"""
+            valid=False
+        if content=='':
+            content_error="""Where is your blog post? I don't see it anywhere!"""
+            valid=False
+        if valid is False: 
+            return render_template('addnew.html', title_error=title_error, content_error=content_error)
+        blog = Bloggz(title, content)
+        db.session.add(blog)
+        db.session.commit()
+        curr_id = str(blog.id)
+    return redirect('/ind_blog?id='+curr_id)
+
+@app.route('/ind_blog', methods=['POST', 'GET'])
+def blog():
+    id = request.args['id']
+    post = Bloggz.query.filter_by(id=id).first()
+    return render_template('/ind_blog.html', post=post)
+
+@app.route('/add_new', methods=['POST', 'GET'])
+def add_page():
+    return render_template('/addnew.html')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
-        db.session.commit()
-
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Get It Done!", 
-        tasks=tasks, completed_tasks=completed_tasks)
-
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
-
+    posts = (reversed(Bloggz.query.all()))
+    
+    return render_template('blog.html', posts=posts)
 
 if __name__ == '__main__':
-app.run()
+    app.run()
